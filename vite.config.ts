870039a9f2eefc38723a4732647e988814c4b019
CDT Ishaan <percyjackson7.riptide@gmail.com>
2025-10-19 15:33:00 +0530

    Add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
